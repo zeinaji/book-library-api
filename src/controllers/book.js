203be254769e7c9ea846c4838dc40ffe4ae0ecd8
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-05-18 09:35:12 +0100

    Refactor book to use generalised helpers

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,63 +1,21 @@
-const { Book } = require('../models');
+const {
+  getAllItems,
+  createItem,
+  updateItem,
+  getItemById,
+  deleteItem,
+} = require('./helpers');
 
-const getBooks = (_, res) => {
-  Book.findAll().then((books) => {
-    res.status(200).json(books);
-  });
-};
+const getBooks = (_, res) => getAllItems(res, 'book');
 
-const createBook = (req, res) => {
-  const newBook = req.body;
+const createBook = (req, res) => createItem(res, 'book', req.body);
 
-  Book.create(newBook)
-    .then((newBookCreated) => res.status(201).json(newBookCreated))
-    .catch((error) => {
-      const errorMessages = error.errors.map((e) => e.message);
+const updateBook = (req, res) =>
+  updateItem(res, 'book', req.body, req.params.id);
 
-      return res.status(400).json({ errors: errorMessages });
-    });
-};
+const getBookById = (req, res) => getItemById(res, 'book', req.params.id);
 
-const updateBook = (req, res) => {
-  const { id } = req.params;
-  const newDetails = req.body;
-
-  Book.update(newDetails, { where: { id } }).then(([recordsUpdated]) => {
-    if (!recordsUpdated) {
-      res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      Book.findByPk(id).then((updatedBook) => {
-        res.status(200).json(updatedBook);
-      });
-    }
-  });
-};
-
-const getBookById = (req, res) => {
-  const { id } = req.params;
-
-  Book.findByPk(id).then((book) => {
-    if (!book) {
-      res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      res.status(200).json(book);
-    }
-  });
-};
-
-const deleteBook = (req, res) => {
-  const { id } = req.params;
-
-  Book.findByPk(id).then((foundBook) => {
-    if (!foundBook) {
-      res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      Book.destroy({ where: { id } }).then(() => {
-        res.status(204).send();
-      });
-    }
-  });
-};
+const deleteBook = (req, res) => deleteItem(res, 'book', req.params.id);
 
 module.exports = {
   getBooks,
