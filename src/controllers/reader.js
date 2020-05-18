2fb8bc78df3758ee1fe6de8a9d8bc48917ba97d3
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-05-18 09:34:59 +0100

    Refactor reader controller to use generalised helpers

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -1,63 +1,21 @@
-const { Reader } = require('../models');
+const {
+  getAllItems,
+  createItem,
+  updateItem,
+  getItemById,
+  deleteItem,
+} = require('./helpers');
 
-const getReaders = (_, res) => {
-  Reader.findAll().then((readers) => {
-    res.status(200).json(readers);
-  });
-};
+const getReaders = (_, res) => getAllItems(res, 'reader');
 
-const createReader = (req, res) => {
-  const newReader = req.body;
+const createReader = (req, res) => createItem(res, 'reader', req.body);
 
-  Reader.create(newReader)
-    .then((newReaderCreated) => res.status(201).json(newReaderCreated))
-    .catch((error) => {
-      const errorMessages = error.errors.map((e) => e.message);
+const updateReader = (req, res) =>
+  updateItem(res, 'reader', req.body, req.params.id);
 
-      return res.status(400).json({ errors: errorMessages });
-    });
-};
+const getReaderById = (req, res) => getItemById(res, 'reader', req.params.id);
 
-const updateReader = (req, res) => {
-  const { id } = req.params;
-  const newDetails = req.body;
-
-  Reader.update(newDetails, { where: { id } }).then(([recordsUpdated]) => {
-    if (!recordsUpdated) {
-      res.status(404).json({ error: 'The reader could not be found.' });
-    } else {
-      Reader.findByPk(id).then((updatedReader) => {
-        res.status(200).json(updatedReader);
-      });
-    }
-  });
-};
-
-const getReaderById = (req, res) => {
-  const { id } = req.params;
-
-  Reader.findByPk(id).then((reader) => {
-    if (!reader) {
-      res.status(404).json({ error: 'The reader could not be found.' });
-    } else {
-      res.status(200).json(reader);
-    }
-  });
-};
-
-const deleteReader = (req, res) => {
-  const { id } = req.params;
-
-  Reader.findByPk(id).then((foundReader) => {
-    if (!foundReader) {
-      res.status(404).json({ error: 'The reader could not be found.' });
-    } else {
-      Reader.destroy({ where: { id } }).then(() => {
-        res.status(204).send();
-      });
-    }
-  });
-};
+const deleteReader = (req, res) => deleteItem(res, 'reader', req.params.id);
 
 module.exports = {
   getReaders,
