238c41c6c774948bf29b845d39b984731d8f13a4
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-05-18 12:21:36 +0100

    Remove  password from responses and update test

--- tests/readers.test.js
+++ tests/readers.test.js
@@ -21,12 +21,15 @@ describe('/readers', () => {
 
         expect(response.status).to.equal(201);
         expect(response.body.name).to.equal('Elizabeth Bennet');
+        expect(response.body.email).to.equal('future_ms_darcy@gmail.com');
+        expect(response.body.password).to.equal(undefined);
+
         expect(newReaderRecord.name).to.equal('Elizabeth Bennet');
         expect(newReaderRecord.email).to.equal('future_ms_darcy@gmail.com');
         expect(newReaderRecord.password).to.equal('iamabadpassword');
       });
 
-      it('errors if an email or password is in the wrong format', async () => {
+      it('errors if an email or password are in the wrong format', async () => {
         const response = await request(app).post('/readers').send({
           name: 'Elizabeth Bennet',
           email: 'future_ms_darcygmail.com',
@@ -51,7 +54,8 @@ describe('/readers', () => {
         expect(response.body.errors.length).to.equal(3);
         expect(newReaderRecord).to.equal(null);
       });
-      it('errors is name is an empty string', async () => {
+
+      it('errors if name is an empty string', async () => {
         const response = await request(app).post('/readers').send({
           name: '',
           password: '12345667895678',
@@ -105,7 +109,7 @@ describe('/readers', () => {
 
           expect(reader.name).to.equal(expected.name);
           expect(reader.email).to.equal(expected.email);
-          expect(reader.password).to.equal(expected.password);
+          expect(reader.password).to.equal(undefined);
         });
       });
     });
@@ -118,6 +122,7 @@ describe('/readers', () => {
         expect(response.status).to.equal(200);
         expect(response.body.name).to.equal(reader.name);
         expect(response.body.email).to.equal(reader.email);
+        expect(response.body.password).to.equal(undefined);
       });
 
       it('returns a 404 if the reader does not exist', async () => {
